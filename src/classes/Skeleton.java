746808bdbc126428cae2b1b87f5b75fa0d8e1413
ventt <adrian@szell.dev>
2021-03-20 20:20:23 +0100

    Try, catch bugfix

--- src/classes/Skeleton.java
+++ src/classes/Skeleton.java
@@ -4,11 +4,11 @@ import java.io.*;
 
 public class Skeleton{
 
-    public void runUsecase(String line) {
-        try {
-            int num = Integer.parseInt(line);
+    public void runUsecase(String line) throws NumberFormatException {
+        int num = Integer.parseInt(line);
             if (num == 1) {
                 //exit(cmd);
+                System.out.println("Ez az elso parancs.");
             } else if (num == 2) {
                 //pwd(cmd);
             } else if (num == 3) {
@@ -16,8 +16,6 @@ public class Skeleton{
             } else {
                 System.out.println("Invalid command");
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+
     }
 }
