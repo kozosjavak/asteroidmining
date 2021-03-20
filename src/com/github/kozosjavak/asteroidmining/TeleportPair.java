979914667fa6ae60286f55469fa7cd5d40e317e7
Marcell Galatean <galateanmarcell96@gmail.com>
2021-03-21 00:11:34 +0100

    teleportPair implementálva closes #9

---
+++ src/com/github/kozosjavak/asteroidmining/TeleportPair.java
@@ -0,0 +1,21 @@
+package com.github.kozosjavak.asteroidmining;
+
+public class TeleportPair {
+
+    private Location location1 = null;
+    private Location location2 = null;
+
+    public void deployTeleport(Location location) {
+        if (location1 == null) {
+            location1 = location;
+        } else {
+            location2 = location;
+            activateTeleport();
+        }
+    }
+
+    public void activateTeleport() {
+        location1.AddNeighbor(location2);
+        location2.AddNeighbor(location1);
+    }
+}
