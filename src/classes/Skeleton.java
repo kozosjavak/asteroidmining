b40e9cfcda3d8ac9b8effe687d5e1710b78ce5d0
Marcell Galatean <galateanmarcell96@gmail.com>
2021-03-20 19:42:52 +0100

    vaalmi

--- src/classes/Skeleton.java
+++ src/classes/Skeleton.java
@@ -1,16 +1,15 @@
 package classes;
 
+import java.io.*;
+
 public class Skeleton{
-<<<<<<< HEAD
-    
-=======
 
     public void runUsecase(String line) {
         try {
             int num = Integer.parseInt(line);
             if (num == 1) {
                 //exit(cmd);
-            } else if (num = 2) {
+            } else if (num == 2) {
                 //pwd(cmd);
             } else if (num == 3) {
                 //cd(cmd);
@@ -21,5 +20,4 @@ public class Skeleton{
             ex.printStackTrace();
         }
     }
->>>>>>> 0771e130c2dfa7aca2d69f4d85a87e95a83ce35c
 }
