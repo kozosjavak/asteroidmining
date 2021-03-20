7d8baa947e00bfbc092fda7b7d16b5e17bfd715e
Marcell Galatean <galateanmarcell96@gmail.com>
2021-03-20 23:20:19 +0100

    timer implementálva closes #6

---
+++ src/classes/Timer.java
@@ -0,0 +1,22 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Timer {
+    private final Collection<Steppable> steppables = new ArrayList<Steppable>();
+
+    public void addSteppable(Steppable steppable) {
+        steppables.add(steppable);
+    }
+
+    public void removeSteppable(Steppable steppable) {
+        steppables.remove(steppable);
+    }
+
+    public void tick() {
+        for (Steppable steppable : steppables) {
+            steppable.step();
+        }
+    }
+}
