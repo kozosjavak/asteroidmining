c02fe7d299e9a4d4d631db1d0989c0c48e76fcbf
ventt <adrian@szell.dev>
2021-03-20 23:25:29 +0100

    Game class letrehozasa (Szingleton helyett staticus fgv.ek) closes #8

---
+++ src/classes/Game.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Game {
+    private static final Sun theSun = new Sun(5);
+    private static int numberOfSetlers;
+
+    public static void startGame() {
+    }
+
+    public static void endGame() {
+    }
+}
