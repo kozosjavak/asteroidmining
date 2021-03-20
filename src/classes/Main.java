0771e130c2dfa7aca2d69f4d85a87e95a83ce35c
Marcell Galatean <galateanmarcell96@gmail.com>
2021-03-20 19:13:43 +0100

    menu

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -1,11 +1,11 @@
-package classes
+package classes;
 
 import java.io.*;
 
 public class Main {
 
     public static void main(String args[]) {
-        //FileFun filefun = new FileFun();
+        Skeleton skeleton = new Skeleton();
 
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
@@ -13,27 +13,10 @@ public class Main {
             while (true) {
                 String line = br.readLine();
                 if (line == null) break;
-                //filefun.readFun(line);
+                skeleton.runUsecase(line);
             }
             br.close();
         } catch(IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void readFun(String line) {
-        try {
-            String[] cmd = line.split(" ");
-            if (cmd[0].equals("exit")) {
-                exit(cmd);
-            } else if (cmd[0].equals("pwd")) {
-                pwd(cmd);
-            } else if (cmd[0].equals("cd")) {
-                cd(cmd);
-            } else {
-                System.out.println("Invalid command");
-            }
-        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
--- src/classes/Skeleton.java
+++ src/classes/Skeleton.java
@@ -1,5 +1,21 @@
-package classes
+package classes;
 
 public class Skeleton{
 
+    public void runUsecase(String line) {
+        try {
+            int num = Integer.parseInt(line);
+            if (num == 1) {
+                //exit(cmd);
+            } else if (num = 2) {
+                //pwd(cmd);
+            } else if (num == 3) {
+                //cd(cmd);
+            } else {
+                System.out.println("Invalid command");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
