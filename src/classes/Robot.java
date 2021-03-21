3b91ac88f816c58b49d4f18d6149f1807ca7098f
Guby002 <75545009+Guby002@users.noreply.github.com>
2021-03-21 03:25:46 +0100

    override, es par fuggveny probalkozas closes #5

--- src/classes/Robot.java
+++ src/classes/Robot.java
@@ -2,9 +2,16 @@ package classes;
 
 public class Robot extends Spaceship implements Steppable{
 
+    @Override
     public void ExperienceSolarStorm(){};
+
+    @Override
     public void GetHitByExplosion(){};
+
+    @Override
     public void Drill(){};
+
+    @Override
     public String GetType(){
         return "robot";
     }
