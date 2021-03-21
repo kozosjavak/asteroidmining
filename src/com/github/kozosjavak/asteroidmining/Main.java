aecd5f099dea7a285887ed0486f30df1ab6c9595
Marcell Galatean <galateanmarcell96@gmail.com>
2021-03-21 16:59:06 +0100

    menu kialakítása

--- src/com/github/kozosjavak/asteroidmining/Main.java
+++ src/com/github/kozosjavak/asteroidmining/Main.java
@@ -8,25 +8,43 @@ public class Main {
 
     public static void main(String[] args) {
         Skeleton skeleton = new Skeleton();
+
         try {
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
 
             while (true) {
                 try {
+                    printOptions();
                     String line = br.readLine();
                     if (line == null) break;
                     skeleton.runUsecase(line);
-                } catch (NumberFormatException ex){
-                    System.out.println("A megadott parancs nem szam.");
-                    //ex.printStackTrace();
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
                 }
+                System.out.println();
             }
 
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void printOptions() {
+        System.out.println("Use-case:");
+        System.out.println("1. Aszteroida fúrása");
+        System.out.println("2. Játék elvesztése");
+        System.out.println("3. Játék megnyerése");
+        System.out.println("4. Nyersanyag bányászása");
+        System.out.println("5. Nyersanyag visszahelyezése");
+        System.out.println("6. Radioaktív aszteroida fúrás");
+        System.out.println("7. Robot építés");
+        System.out.println("8. Telepes mozgatása");
+        System.out.println("9. Teleportkapu lehelyezése");
+        System.out.println("10. Teleportkapu-pár építése");
+        System.out.println("11. Üreges aszteroidából nyersanyag felvétel");
+        System.out.println("12. Űrbázis építése");
+        System.out.print("Melyik use-case-t szeretné futtatni? Adjon meg egy számot: ");
     }
 }
--- src/com/github/kozosjavak/asteroidmining/Skeleton.java
+++ src/com/github/kozosjavak/asteroidmining/Skeleton.java
@@ -1,20 +1,90 @@
 package com.github.kozosjavak.asteroidmining;
 
-public class Skeleton{
+public class Skeleton {
 
-    public void runUsecase(String line) throws NumberFormatException {
-        int num = Integer.parseInt(line);
-            if (num == 1) {
-                System.out.println("Ez a komment helye volt.");
-                System.out.println("Ez az elso parancs.");
-            } else if (num == 2) {
-                System.out.println("Ez a masodik parancs.");
-                //pwd(cmd);
-            } else if (num == 3) {
-                //cd(cmd);
-            } else {
-                System.out.println("Invalid command");
-            }
+    public void runUsecase(String line) throws Exception {
+
+        int num = 0;
+        try {
+            num = Integer.parseInt(line);
+        } catch (NumberFormatException ex) {
+            throw new Exception("A megadott parancs nem szám!");
+        }
+
+        if (num == 1) {
+            usecaseDrillAsteroid();
+        } else if (num == 2) {
+            usecaseLoseGame();
+        } else if (num == 3) {
+            usecaseWinGame();
+        } else if (num == 4) {
+            usecaseMineMaterial();
+        } else if (num == 5) {
+            usecaseInserMaterial();
+        } else if (num == 6) {
+            usecaseDrillRadioactiveAsteroid();
+        } else if (num == 7) {
+            usecaseBuildRobot();
+        } else if (num == 8) {
+            usecaseMoveSettler();
+        } else if (num == 9) {
+            usecaseDeployTeleport();
+        } else if (num == 10) {
+            usecaseBuildTeleport();
+        } else if (num == 11) {
+            usecaseRemoveMaterial();
+        } else if (num == 12) {
+            usecaseBuildBase();
+        } else {
+            throw new Exception("Nem megfelelő számot adott meg!");
+        }
+    }
+
+    public void usecaseDrillAsteroid() {
+
+    }
+
+    public void usecaseLoseGame() {
+
+    }
+
+    public void usecaseWinGame() {
+
+    }
+
+    public void usecaseMineMaterial() {
+
+    }
+
+    public void usecaseInserMaterial() {
+
+    }
+
+    public void usecaseDrillRadioactiveAsteroid() {
+
+    }
+
+    public void usecaseBuildRobot() {
+
+    }
+
+    public void usecaseMoveSettler() {
+
+    }
+
+    public void usecaseDeployTeleport() {
+
+    }
+
+    public void usecaseBuildTeleport() {
+
+    }
+
+    public void usecaseRemoveMaterial() {
+
+    }
+
+    public void usecaseBuildBase() {
 
     }
 }
