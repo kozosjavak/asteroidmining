1ec49f4086a74cb1c8981b44dddfc64b1c9c5b74
ventt <adrian@szell.dev>
2021-03-24 12:55:19 +0100

    Maven hozzaadva a projekthez

--- src/test/java/com/github/kozosjavak/asteroidmining/bills/BillOfMaterialTest.java
+++ src/test/java/com/github/kozosjavak/asteroidmining/bills/BillOfMaterialTest.java
@@ -2,44 +2,45 @@ package com.github.kozosjavak.asteroidmining.bills;
 
 import com.github.kozosjavak.asteroidmining.materials.Material;
 import com.github.kozosjavak.asteroidmining.materials.Materials;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class BillOfMaterialTest {
+
+public class BillOfMaterialTest {
 
     @Test
-    void isNeeded_emptyInventory() {
+    public void isNeeded_emptyInventory() {
         Map<Material, Integer> inventory = Map.of();
         Map<Material, Integer> result = Bills.TELEPORT.isNeeded(inventory);
         assertEquals(3, result.size());
-        assertEquals(2, result.get(Materials.IRON));
-        assertEquals(1, result.get(Materials.WATERICE));
-        assertEquals(1, result.get(Materials.URANIUM));
+        assertEquals(2, (int) result.get(Materials.IRON));
+        assertEquals(1, (int) result.get(Materials.WATERICE));
+        assertEquals(1, (int) result.get(Materials.URANIUM));
     }
 
     @Test
-    void isNeeded_exactMatch() {
+    public void isNeeded_exactMatch() {
         Map<Material, Integer> inventory = Map.of(Materials.IRON, 2, Materials.WATERICE, 1, Materials.URANIUM, 1);
         Map<Material, Integer> result = Bills.TELEPORT.isNeeded(inventory);
         assertEquals(0, result.size());
     }
 
     @Test
-    void isNeeded_more() {
+    public void isNeeded_more() {
         Map<Material, Integer> inventory = Map.of(Materials.IRON, 2, Materials.WATERICE, 2, Materials.URANIUM, 1);
         Map<Material, Integer> result = Bills.TELEPORT.isNeeded(inventory);
         assertEquals(0, result.size());
     }
 
     @Test
-    void isNeeded_less() {
+    public void isNeeded_less() {
         Map<Material, Integer> inventory = Map.of(Materials.IRON, 1, Materials.WATERICE, 2, Materials.URANIUM, 1);
         Map<Material, Integer> result = Bills.TELEPORT.isNeeded(inventory);
         assertEquals(1, result.size());
-        assertEquals(1, result.get(Materials.IRON));
+        assertEquals(1, (int) result.get(Materials.IRON));
     }
 
 
