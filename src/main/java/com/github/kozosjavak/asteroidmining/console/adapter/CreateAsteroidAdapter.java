4c1c6a9735025d7cd3e9ef637b24c2cb2578904d
ventt <adrian@szell.dev>
2021-03-31 20:28:02 +0200

    Teszt mappak closes #55

---
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/CreateAsteroidAdapter.java
@@ -0,0 +1,14 @@
+package com.github.kozosjavak.asteroidmining.console.adapter;
+
+import com.github.kozosjavak.asteroidmining.core.commands.Command;
+
+public class CreateAsteroidAdapter implements StringCommandAdapter {
+
+    @Override
+    public Command parse(String str) {
+        String[] hulladek = str.split(" ");
+
+
+        return null;
+    }
+}
