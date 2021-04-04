75e3457cadb8789861b6472027d2c1efe6158114
Szalai András <szalaiandristelo@gmail.com>
2021-04-04 18:33:37 +0200

    Orb test, closes #59

---
+++ src/test/java/com/github/kozosjavak/asteroidmining/core/OrbTest.java
@@ -0,0 +1,36 @@
+package com.github.kozosjavak.asteroidmining.core;
+
+import com.github.kozosjavak.asteroidmining.core.materials.NotEnoughMaterialException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrbTest {
+
+    // Init test locations
+    private Location testLocation1 = new Location();
+    private Location testLocation2 = new Location();
+
+    @Test
+    public void it_should_return_the_set_location_and_change_to_the_other()  {
+        Orb testOrb = new Orb(testLocation1);
+        assertEquals(testLocation1, testOrb.getLocation());
+
+        testOrb.setLocation(testLocation2);
+        assertEquals(testLocation2, testOrb.getLocation());
+    }
+
+    @Test
+    public void exprerience_solar_storm_test()  {
+        /**
+         * Orb.experienceSolarStrom() is not implemented yet
+         */
+    }
+
+    @Test
+    public void exprerience_extreme_heat_test() throws NotEnoughMaterialException {
+        /**
+         * Orb.experienceExtremeHeat() is not implemented yet
+         */
+    }
+}
