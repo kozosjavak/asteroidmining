6de2cf99c2fd19eb52973a82e2954de6ffe88d22
Marcell Galatean <galateanmarcell96@gmail.com>
2021-04-07 20:36:34 +0200

    timer szálkezelés implementációja

--- src/main/java/com/github/kozosjavak/asteroidmining/Timer.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/Timer.java
@@ -8,13 +8,20 @@ import java.util.Collection;
 /**
  * Időzítő osztály
  */
-public class Timer {
+public class Timer implements Runnable {
 
-    /** Léptethető elemek listája a játékban */
-    private final Collection<Steppable> steppables = new ArrayList<Steppable>();
+    private static final int TARGET_FPS = 60;
+    private static final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+    /**
+     * Léptethető elemek listája a játékban
+     */
+    private final Collection<Steppable> steppables = new ArrayList<>();
+    private Thread thread;
+    private boolean isRunning = false;
 
     /**
      * Léptethető elem hozzáadása a listához
+     *
      * @param steppable a hozzáadandó elem
      */
     public void addSteppable(Steppable steppable) {
@@ -35,6 +42,43 @@ public class Timer {
     public void tick() {
         for (Steppable steppable : steppables) {
             steppable.step();
+        }
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        isRunning = true;
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            isRunning = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        long now;
+        long elapsedTime;
+        long wait;
+        while (isRunning) {
+            now = System.nanoTime();
+
+            tick();
+            //pain();
+
+            elapsedTime = System.nanoTime() - now;
+            wait = (OPTIMAL_TIME - elapsedTime) / 1000000;
+
+            try {
+                if (wait < 0) wait = 0;
+                Thread.sleep(wait);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
