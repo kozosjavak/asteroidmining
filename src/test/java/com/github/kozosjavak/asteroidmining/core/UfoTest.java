32a69aed6b4d5d5a409c96c80fb9bc80ebdb8cbc
Marcell Galatean <galateanmarcell96@gmail.com>
2021-04-07 23:09:29 +0200

    ufo experience extreme heat test closes #96

--- src/test/java/com/github/kozosjavak/asteroidmining/core/UfoTest.java
+++ src/test/java/com/github/kozosjavak/asteroidmining/core/UfoTest.java
@@ -1,7 +1,9 @@
 package com.github.kozosjavak.asteroidmining.core;
 
 import com.github.kozosjavak.asteroidmining.core.materials.InventoryIsFullException;
+import com.github.kozosjavak.asteroidmining.core.materials.NotEnoughMaterialException;
 import com.github.kozosjavak.asteroidmining.core.materials.types.Coal;
+import com.github.kozosjavak.asteroidmining.core.materials.types.Uranium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class UfoTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -38,5 +41,17 @@ public class UfoTest {
         Ufo ufo = new Ufo(asteroid1);
         ufo.mine();
         assertEquals("Coal", ufo.getInventory().getList().get(0).toString());
+    }
+
+    @Test
+    public void ufo_get_experience_extreme_heat() throws InventoryIsFullException, NotEnoughMaterialException {
+        Location location1 = new Location(game, 5.4, 3.2);
+        Asteroid asteroid1 = new Asteroid(location1, 0, false, new Coal(), 1);
+        Ufo ufo = new Ufo(asteroid1);
+        ufo.getInventory().add(new Uranium());
+        ufo.experienceExtremeHeat();
+        ufo.experienceExtremeHeat();
+        ufo.experienceExtremeHeat();
+        assertNull(ufo.getCurrentAsteroid());
     }
 }
