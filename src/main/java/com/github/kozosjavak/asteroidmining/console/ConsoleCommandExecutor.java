0032899cf1065a2a1e99b08b74e2f1212cb3f8c7
ventt <adrian@szell.dev>
2021-04-17 21:13:36 +0200

    A commandok mukodose: Atnezest ajanlom a ConsoleCommandExecutor -> CommandExecutor -> StringCommandAdapter -> CreateAsteroidAdapter -> CreateAsteroidCommand Nagyjabol ertheto commentek.

--- src/main/java/com/github/kozosjavak/asteroidmining/console/ConsoleCommandExecutor.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/ConsoleCommandExecutor.java
@@ -1,5 +1,6 @@
 package com.github.kozosjavak.asteroidmining.console;
 
+import com.github.kozosjavak.asteroidmining.console.adapter.CreateAsteroidAdapter;
 import com.github.kozosjavak.asteroidmining.console.adapter.StartCommandAdapter;
 import com.github.kozosjavak.asteroidmining.console.adapter.StringCommandAdapter;
 import com.github.kozosjavak.asteroidmining.core.Game;
@@ -15,20 +16,23 @@ import java.util.Scanner;
  * Console communicator
  */
 public class ConsoleCommandExecutor extends CommandExecutor {
-    private static final List<StringCommandAdapter> commandList = List.of(
-            new StartCommandAdapter()
+    //Ird be a listaba az uj adaptert.
+    private static final List<StringCommandAdapter> adapterCommandList = List.of(
+            new StartCommandAdapter(),
+            new CreateAsteroidAdapter()
     );
 
     public ConsoleCommandExecutor(Game game) {
         super(game);
     }
 
+    //Magic
     public void attachToConsole() {
 
         try (Scanner scanner = new Scanner(System.in)) {
 
             while (scanner.hasNextLine()) {
-                Optional<Command> optionalCommand = commandList.stream()
+                Optional<Command> optionalCommand = adapterCommandList.stream()
                         .map(stringCommandAdapter -> stringCommandAdapter.parse(scanner.nextLine()))
                         .filter(Objects::nonNull)
                         .findAny();
--- src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/CreateAsteroidAdapter.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/CreateAsteroidAdapter.java
@@ -1,14 +1,23 @@
 package com.github.kozosjavak.asteroidmining.console.adapter;
 
 import com.github.kozosjavak.asteroidmining.core.commands.Command;
+import com.github.kozosjavak.asteroidmining.core.commands.CreateAsteroidCommand;
 
+
+//leterhozunk egy create asteroid commandot felismero command adaptert, azaz ez a class dolgozza fel a bejovo Stringet ami tartalmazza az adatokat a System.inbol
 public class CreateAsteroidAdapter implements StringCommandAdapter {
-
+    //Parse fgvt kell felulirni mindenhol
     @Override
     public Command parse(String str) {
-        String[] hulladek = str.split(" ");
+        String[] splitted = str.split(" ");
+        //Nagyon fontos, hogy itt kell egy ellenorzes hogy magara ismere-e a command, azaz itt a peldanak okaert: "create asteroid", meg kell nezzuk hogy a string eleje tartalmazzae a commandot
+        if (splitted[0].equals("CreateAsteroid")) {
+            //ha tartalmazza akkor itt adunk vissza egy uj commandot, amely construktoranak mar atadtuk a megfelelo adatokat a megfelelo formaban.
 
-
+            //leterhozunk egy uj commandot az adatokkal megadva, es visszaadjuk a ConsoleCommandExecutornak, aminek a CommandExecutor foosztalya eltarolja az uj commandot
+            return new CreateAsteroidCommand(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[2]));
+        }
+        //Ha nem a "create asteroid" a string akkor szimplan nullt visszaadva a ConsoleCommandExecutor tudja hogy nem talalt a latogatas (visitor pattern).
         return null;
     }
 }
