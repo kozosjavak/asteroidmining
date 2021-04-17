c1f1d123b449156fb82fe49862b4f12edf149da0
ventt <adrian@szell.dev>
2021-04-17 21:13:36 +0200

    A commandok mukodose: Atnezest ajanlom a ConsoleCommandExecutor -> CommandExecutor -> StringCommandAdapter -> CreateAsteroidAdapter -> CreateAsteroidCommand Nagyjabol ertheto commentek.

--- src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/StringCommandAdapter.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/StringCommandAdapter.java
@@ -1,7 +1,7 @@
 package com.github.kozosjavak.asteroidmining.console.adapter;
 
 import com.github.kozosjavak.asteroidmining.core.commands.Command;
-
+//Mindig ezt az interfacet add a command adapterhez
 public interface StringCommandAdapter {
     Command parse(String str);
 }
--- src/main/java/com/github/kozosjavak/asteroidmining/core/commands/CommandExecutor.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/core/commands/CommandExecutor.java
@@ -8,7 +8,9 @@ import java.util.Collections;
 import java.util.List;
 
 public abstract class CommandExecutor {
+    //Jatekot tarolja amin dolgozunk
     private final Game game;
+    //itt tarolja az elkeszult commandokat
     private final List<Command> executedCommands = new ArrayList<>();
 
     public CommandExecutor(Game game) {
@@ -19,17 +21,20 @@ public abstract class CommandExecutor {
         return game;
     }
 
+    //itt tudsz egy bizonyos commandot lefuttatni
     public void execute(Command command) throws Exception {
         command.apply(game);
         executedCommands.add(command);
     }
 
+    //lefuttatja a tarolt osszes commandot
     public void executeAll(Collection<Command> commands) throws Exception {
         for (Command command : commands) {
             execute(command);
         }
     }
 
+    //visszaadja a listat egy modosithatlan listaban
     public List<Command> getExecutedCommands() {
         return Collections.unmodifiableList(executedCommands);
     }
