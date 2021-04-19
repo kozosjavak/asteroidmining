2735bc3cb65aa38e49da5d0781a3753e7a5212a3
Szalai András <szalaiandristelo@gmail.com>
2021-04-19 11:50:47 +0200

    SettlerMineMaterialCommand, closes #114

--- src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/SettlerMineAdapter.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/SettlerMineAdapter.java
@@ -1,15 +1,15 @@
 package com.github.kozosjavak.asteroidmining.console.adapter;
 
 import com.github.kozosjavak.asteroidmining.core.commands.Command;
-import com.github.kozosjavak.asteroidmining.core.commands.SettlerDrillCommand;
+import com.github.kozosjavak.asteroidmining.core.commands.SettlerMineCommand;
 
-public class SettlerDrillAdapter implements StringCommandAdapter {
+public class SettlerMineAdapter implements StringCommandAdapter {
     @Override
     public Command parse(String str) {
         String[] splitted = str.split(" ");
-        if (splitted[0].equals("SettlerDrill")) {
+        if (splitted[0].equals("SettlerMine")) {
             if (splitted.length > 2) {
-                return new SettlerDrillCommand(Integer.parseInt(splitted[1]));
+                return new SettlerMineCommand(Integer.parseInt(splitted[1]));
             } else {
                 System.out.println("Not enough IDs for the command");
             }
