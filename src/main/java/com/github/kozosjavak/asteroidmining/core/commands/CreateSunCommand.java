b7a5a12a664fe7192e394e430ab00678625f41c0
Szalai András <szalaiandristelo@gmail.com>
2021-04-19 11:19:06 +0200

    CreateSunCommand, closes #106

---
+++ src/main/java/com/github/kozosjavak/asteroidmining/core/commands/CreateSunCommand.java
@@ -0,0 +1,29 @@
+package com.github.kozosjavak.asteroidmining.core.commands;
+
+import com.github.kozosjavak.asteroidmining.core.Game;
+import com.github.kozosjavak.asteroidmining.core.Location;
+import com.github.kozosjavak.asteroidmining.core.Sun;
+
+/**
+ * Nap letrehozasa parancs implementacio
+ */
+public class CreateSunCommand implements Command {
+
+    /**
+     * Parancs ctor
+     */
+    public CreateSunCommand() { }
+
+    /**
+     * Parancs applikalasa a jatekra
+     * @param game a jatek melyre alkalmazzuk
+     * @throws Exception
+     */
+    @Override
+    public void apply(Game game) throws Exception {
+        Location loc = new Location(game, 0.0, 0.0);
+        Sun sun = new Sun(loc);
+        game.addLocation(loc);
+        game.putInIdList(sun);
+    }
+}
