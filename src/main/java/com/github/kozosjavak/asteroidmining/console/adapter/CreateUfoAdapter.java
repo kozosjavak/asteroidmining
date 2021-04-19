e014b9f53123615d6036e9cd683b359290dda073
Szalai András <szalaiandristelo@gmail.com>
2021-04-19 11:57:48 +0200

    CreateUfoCommand, closes #116

---
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/CreateUfoAdapter.java
@@ -0,0 +1,18 @@
+package com.github.kozosjavak.asteroidmining.console.adapter;
+
+import com.github.kozosjavak.asteroidmining.core.commands.Command;
+import com.github.kozosjavak.asteroidmining.core.commands.CreateSettlerCommand;
+
+public class CreateUfoAdapter implements StringCommandAdapter {
+    @Override
+    public Command parse(String str) {
+        String[] splitted = str.split(" ");
+        if (splitted[0].equals("CreateUfo")) {
+            if (splitted.length == 2) {
+                return new CreateSettlerCommand(Integer.parseInt(splitted[1]));
+            }
+            System.out.println("No id has given!\n");
+        }
+        return null;
+    }
+}
