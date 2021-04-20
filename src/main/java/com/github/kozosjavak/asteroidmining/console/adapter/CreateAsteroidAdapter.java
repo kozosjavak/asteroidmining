1a44425582c03e3ddd03acd669557fc7c8a79ef6
Szalai András <szalaiandristelo@gmail.com>
2021-04-20 13:51:11 +0200

    CreateAsteroidCommand & CreateAsteroid Adapter fix, closes 170

--- src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/CreateAsteroidAdapter.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/console/adapter/CreateAsteroidAdapter.java
@@ -14,8 +14,13 @@ public class CreateAsteroidAdapter implements StringCommandAdapter {
         if (splitted[0].equals("CreateAsteroid")) {
             //ha tartalmazza akkor itt adunk vissza egy uj commandot, amely construktoranak mar atadtuk a megfelelo adatokat a megfelelo formaban.
 
-            //leterhozunk egy uj commandot az adatokkal megadva, es visszaadjuk a ConsoleCommandExecutornak, aminek a CommandExecutor foosztalya eltarolja az uj commandot
-            return new CreateAsteroidCommand(Integer.parseInt(splitted[1]));
+            if(splitted.length > 2) { // kapott materialt is
+                //leterhozunk egy uj commandot az adatokkal megadva, es visszaadjuk a ConsoleCommandExecutornak, aminek a CommandExecutor foosztalya eltarolja az uj commandot
+                return new CreateAsteroidCommand(Integer.parseInt(splitted[1]), Integer.parseInt(splitted[2]));
+            }
+            else { // nem kapott materialt
+                return new CreateAsteroidCommand(Integer.parseInt(splitted[1]), -1);
+            }
         }
         //Ha nem a "create asteroid" a string akkor szimplan nullt visszaadva a ConsoleCommandExecutor tudja hogy nem talalt a latogatas (visitor pattern).
         return null;
