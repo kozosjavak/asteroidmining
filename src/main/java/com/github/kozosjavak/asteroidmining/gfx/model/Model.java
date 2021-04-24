19ebba25890e4285f7ea195e41e9268eb1fa63b2
ventt <adrian@szell.dev>
2021-04-25 01:34:21 +0200

    Modellek letrehozasa

---
+++ src/main/java/com/github/kozosjavak/asteroidmining/gfx/model/Model.java
@@ -0,0 +1,22 @@
+package com.github.kozosjavak.asteroidmining.gfx.model;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public class Model {
+    TextureRegion texture;
+    Vector2 position;
+
+    public Model(TextureAtlas atlas, Vector2 position) {
+        this.position = position;
+
+    }
+
+    public void draw(SpriteBatch batch) {
+    }
+
+    public void coreObjectToModelObject() {
+    }
+}
--- src/main/java/com/github/kozosjavak/asteroidmining/gfx/view/AsteroidMiningGame.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/gfx/view/AsteroidMiningGame.java
@@ -5,6 +5,11 @@ import com.badlogic.gdx.Game;
 public class AsteroidMiningGame extends Game {
 
     GameScreen gameScreen;
+    private final com.github.kozosjavak.asteroidmining.core.Game game;
+
+    public AsteroidMiningGame(com.github.kozosjavak.asteroidmining.core.Game game) {
+        this.game = game;
+    }
 
     @Override
     public void create() {
