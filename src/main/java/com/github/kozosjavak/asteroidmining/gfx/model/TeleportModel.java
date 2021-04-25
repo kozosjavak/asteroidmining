e7b3a70e421840027d46c3e8545ab891ffc26b2d
ventt <adrian@szell.dev>
2021-04-25 03:02:46 +0200

    updateModelList

---
+++ src/main/java/com/github/kozosjavak/asteroidmining/gfx/model/TeleportModel.java
@@ -0,0 +1,17 @@
+package com.github.kozosjavak.asteroidmining.gfx.model;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+public class TeleportModel extends Model {
+    public TeleportModel(TextureAtlas atlas, Vector2 position) {
+        super(atlas, position);
+        //teleport texture
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        // TODO
+    }
+}
--- src/main/java/com/github/kozosjavak/asteroidmining/gfx/view/AsteroidMiningGame.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/gfx/view/AsteroidMiningGame.java
@@ -3,26 +3,43 @@ package com.github.kozosjavak.asteroidmining.gfx.view;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
-import com.github.kozosjavak.asteroidmining.core.Location;
-import com.github.kozosjavak.asteroidmining.core.Sun;
-import com.github.kozosjavak.asteroidmining.gfx.model.Model;
-import com.github.kozosjavak.asteroidmining.gfx.model.SunModel;
+import com.github.kozosjavak.asteroidmining.core.*;
+import com.github.kozosjavak.asteroidmining.core.materials.types.Coal;
+import com.github.kozosjavak.asteroidmining.gfx.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AsteroidMiningGame extends Game {
 
-    GameScreen gameScreen;
     private final com.github.kozosjavak.asteroidmining.core.Game game;
     private final TextureAtlas textureAtlas;
+    GameScreen gameScreen;
 
     public AsteroidMiningGame(com.github.kozosjavak.asteroidmining.core.Game game) {
         this.game = game;
-        textureAtlas = new TextureAtlas("images.atlas");
+        textureAtlas = new TextureAtlas("kozosjavak_astpck.atlas");
         Location sunLocation = new Location(game, 0.0, 0.0);
         game.setSun(new Sun(sunLocation));
 
+        Location locationAsteroid = new Location(game, 50.0, 50.0);
+        Asteroid asteroid = new Asteroid(locationAsteroid, 1, new Coal());
+        game.addLocation(locationAsteroid);
+
+        Location locationAsteroidSettler = new Location(game, 250.0, 50.0);
+        Asteroid asteroidSettler = new Asteroid(locationAsteroid, 1, new Coal());
+        Settler settler = new Settler(asteroidSettler);
+        game.addLocation(locationAsteroidSettler);
+
+        Location locationAsteroidUfo = new Location(game, 50.0, 250.0);
+        Asteroid asteroidUfo = new Asteroid(locationAsteroid, 1, new Coal());
+        Ufo ufo = new Ufo(asteroidUfo);
+        game.addLocation(locationAsteroidUfo);
+
+        Location locationAsteroidRobot = new Location(game, 1000.0, 1000.0);
+        Asteroid asteroidRobot = new Asteroid(locationAsteroid, 1, new Coal());
+        Robot robot = new Robot(asteroidRobot);
+        game.addLocation(locationAsteroidRobot);
     }
 
     @Override
@@ -38,7 +55,7 @@ public class AsteroidMiningGame extends Game {
 
     @Override
     public void render() {
-        updateModellList();
+        updateModelList();
         super.render();
     }
 
@@ -51,12 +68,29 @@ public class AsteroidMiningGame extends Game {
      * Olyan sorrendbe kell a listat feltolteni ahogyan azt meg akarjuk jeleniteni
      * Ha nem tudunk castolni floatra akkor at kell irni a gamebe
      */
-    private void updateModellList() {
+    private void updateModelList() {
         List<Model> modelList = new ArrayList<>();
         modelList.add(new SunModel(textureAtlas, new Vector2((float) game.getSun().getLocation().getCoordinate().getX(), (float) game.getSun().getLocation().getCoordinate().getY())));
-        //for (Location location : game.getLocationList()) {
-        //    //aszteroidak, urhajok, portalok
-        //}
+        for (Location location : game.getLocationList()) {
+            if (location.getOrb().getClass() == Asteroid.class && location.getOrb() != null) {
+                modelList.add(new AsteroidModel(textureAtlas, new Vector2((float) location.getCoordinate().getX(), (float) location.getCoordinate().getY())));
+            }
+            if (location.getTeleport() != null) {
+                modelList.add(new TeleportModel(textureAtlas, new Vector2((float) location.getCoordinate().getX(), (float) location.getCoordinate().getY())));
+            }
+            if (location.getOrb().getClass() == Asteroid.class && location.getOrb() != null) {
+                Asteroid asteroid = (Asteroid) location.getOrb();
+                for (Spaceship sp : asteroid.getResidence()) {
+                    if (sp.getClass() == Settler.class) {
+                        modelList.add(new SettlerModel(textureAtlas, new Vector2((float) location.getCoordinate().getX(), (float) location.getCoordinate().getY())));
+                    } else if (sp.getClass() == Ufo.class) {
+                        modelList.add(new UfoModel(textureAtlas, new Vector2((float) location.getCoordinate().getX(), (float) location.getCoordinate().getY())));
+                    } else if (sp.getClass() == Robot.class) {
+                        modelList.add(new RobotModel(textureAtlas, new Vector2((float) location.getCoordinate().getX(), (float) location.getCoordinate().getY())));
+                    }
+                }
+            }
+        }
         gameScreen.updateModelList(modelList);
     }
 
