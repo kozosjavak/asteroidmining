dd364f942f41d8d9cab2bfef4ccce518608ef876
ventt <adrian@szell.dev>
2021-05-10 00:58:12 +0200

    Janoshegyen

--- src/main/java/com/github/kozosjavak/asteroidmining/Main.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/Main.java
@@ -29,7 +29,7 @@ public class Main {
         // GuiEventHandler inputProcessor = new GuiEventHandler();
         //Gdx.input.setInputProcessor(inputProcessor);
         try {
-            game.startGame(160, 10, 100, 1, 3);
+            game.startGame(160, 5, 100, 5, 3);
         } catch (Exception exception) {
             exception.printStackTrace();
         }
