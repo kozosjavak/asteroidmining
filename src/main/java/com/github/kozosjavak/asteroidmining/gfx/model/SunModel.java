b65ba86b394afca566b08c54c0f0e55deeceabb7
ventt <adrian@szell.dev>
2021-05-10 12:08:53 +0200

    napsugar beallitva

--- src/main/java/com/github/kozosjavak/asteroidmining/gfx/model/SunModel.java
+++ src/main/java/com/github/kozosjavak/asteroidmining/gfx/model/SunModel.java
@@ -2,24 +2,37 @@ package com.github.kozosjavak.asteroidmining.gfx.model;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class SunModel extends Model {
     boolean isSunShine;
+    TextureRegion radiance;
+    boolean isRadiant;
+    float sunCenteX;
+    float sunCenterY;
 
     public SunModel(TextureAtlas atlas, Vector2 position, boolean isSunShine) {
         super(atlas, position);
+        radiance = atlas.findRegion("sunStorm");
         this.isSunShine = isSunShine;
         if (isSunShine) {
+            isRadiant = true;
             texture = atlas.findRegion("ssuuuuuunnnn");
         } else {
+            isRadiant = false;
             texture = atlas.findRegion("sun");
         }
+        sunCenteX = position.x - texture.getRegionWidth() / 2;
+        sunCenterY = position.y - texture.getRegionHeight() / 2;
     }
 
     @Override
     public void draw(SpriteBatch batch) {
-        batch.draw(texture, position.x - texture.getRegionWidth(), position.y - texture.getRegionHeight());
+        if (isRadiant) {
+            batch.draw(radiance, sunCenteX - radiance.getRegionWidth() / 2 + 30, sunCenterY - radiance.getRegionHeight() / 2 + 30);
+        }
+        batch.draw(texture, sunCenteX, sunCenterY);
     }
 
     @Override
